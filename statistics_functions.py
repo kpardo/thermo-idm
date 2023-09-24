a5568db89ae4cd297a047d6f73e9964fdd0b3bf5
Eleanor Stuart <eleanorstuart@Eleanors-Air.lan>
2023-09-24 12:11:00 -0700

    2 param mcmc still bad :(

--- statistics_functions.py
+++ statistics_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from astropy import constants as const
+from astropy import units as u
 
 def chi_squared(T_model, T_data, variance): # take lists of model prediction, data, and variance of same length
     chi_squared_sum = 0
@@ -27,8 +28,13 @@ def log_likelihood_1(p0, T_data, var, clusters, m_chi):
 def log_likelihood(p0, T_data, var, clusters):
     #if p0[0]<0 or p0[1]<0:
     #   return -np.inf
-    if p0[0]>0 or p0[1]>0:
+    if p0[0]>0:
         return -np.inf
+    if p0[1]<-10:
+        return -np.inf
+    if 10**p0[1]>const.m_p.to(u.GeV, equivalencies=u.mass_energy()).value:
+        return -np.inf
+
     print(p0)
     T_model = [c.pred_T_b(p0) for c in clusters]
     X2 = chi_squared(T_model, T_data, var)
